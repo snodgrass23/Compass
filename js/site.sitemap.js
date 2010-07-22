284c72387c59b2a3f15a186f87d2c7f4267d8848
Hunter Loftis <hunter@skookum.com>
2010-07-22 17:33:04 -0400

    Implemented using jQuery layout plugin. I think it's overkill.

--- js/site.sitemap.js
+++ js/site.sitemap.js
@@ -12,6 +12,26 @@ SKOOKUM.SM.test_data = new SKOOKUM.SM.NodeData(
 SKOOKUM.SM.test_data = new SKOOKUM.SM.NodeData().generate_random(5);
 
 window.onload = function () {
+	$('body').layout({
+		defaults: {
+			fxName:					"slide",
+			fxSpeed:				200,
+			resizable:				false,
+			slidable:				false,
+			spacing_open:			0,
+			spacing_closed:			15,
+			initClosed:				false,
+			togglerLength_closed:	"15",
+			togglerLength_open:		"0",
+			togglerAlign_closed:	"top"
+		},
+		north: {
+			
+		},
+		east: {
+			initClosed:				false
+		}
+	}).addCloseBtn('#toolbar-closer', 'east');
 	SKOOKUM.SM.map = $("#map").siteMap();
 	//$("#map2").siteMap();
 	SKOOKUM.SM.editor = $("#node-editor").nodeEditor();
